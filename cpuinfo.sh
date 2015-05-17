2cc75bc4033bf195aa4505f988a727b60bd601fd
BiggerMan <flytiger_ouc@sina.com>
2015-05-17 23:44:45 +0800

    script for checking cpu info

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo -n "CPU numbers:"
+echo -n "physical CPU number in total:"
 cat /proc/cpuinfo | grep "processor" | wc -l
 
-echo -n "core numbers in a physical cpu:"
+echo -n "core numbers in a physical CPU:"
 core_per_phy_cpu=$( cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l )
 echo $core_per_phy_cpu
+
+echo -n "logical CPU numbers in a physical CPU:"
+logical_cpu_per_phy_cpu=$(cat /proc/cpuinfo | grep "siblings" | sort | uniq | awk -F: '{print $2}' )
+echo $logical_cpu_per_phy_cpu
+
+if [ $logical_cpu_per_phy_cpu -gt $core_per_phy_cpu ];then
+	echo "Hyper threading is enabled"
+elif [ $logical_cpu_per_phy_cpu -eq $core_per_phy_cpu ];then
+	echo "Hyper threading is NOT enabled"
+else
+	echo "Error!there is someting working wrong..."
+fi
