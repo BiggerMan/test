453811438ecbba749cba9677896a605e4db2fa27
BiggerMan <flytiger_ouc@sina.com>
2015-05-22 22:38:36 +0800

    how to generate *.so and use it

---
+++ libtest/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "test.h"
+
+int main()
+{
+	printf("how lib.so works??\n");
+	testFunc();
+
+	return 0;
+}
+
---
+++ libtest/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "test.h"
+
+int testFunc(void)
+{
+	printf("this implements in lib\n");
+
+	return 0;
+}
